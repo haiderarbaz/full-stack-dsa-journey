199da12204b1bf83dccab766501d130dea1ec732
haiderarbaz <admin@arbazhaider.com>
2026-08-20 11:48:49 +0000

    Day 2: DSA intro - C++ basics, first Hello World program

---
+++ dsa/day02-hello-world.cpp
@@ -0,0 +1,28 @@
+/* First Way */
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main (){
+
+//   cout << "Hello World ! \n";
+
+//   // "\n" will move the cursor to new line
+
+// }
+
+/* Second Way */
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  cout << "Hello World !" << endl;
+  
+  cout << "Hello World !" << " using CPP" << " programming languages." << endl;
+
+  cout << "Hello World !" << endl << "using CPP" << endl << "programming languages." << endl;
+
+  cout << "Hi \n" << "How you doing? \n" << "When you're coming here? \n";
+  
+}
+
